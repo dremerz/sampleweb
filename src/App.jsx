7ff596f6be126d7de385c6d3ec8c9d148286a3f5
aravind <dremerz.product@gmail.com>
2023-07-23 19:18:24 +0530

    m

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Home';
+import Test from './Test';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      
+        <Route index element={<Home />} />
+      
+    </Routes>
+  </BrowserRouter>
+  )
+}

0f5e9a96903b9852109a14f637d071ee20e8bf60
aravind <dremerz.product@gmail.com>
2023-07-23 19:39:58 +0530

    m

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>down <br></br>  <a href="/test"> click </a></div>
+    <div>down <br></br> <Link to="/test">Blogs</Link></div>
 
   )
 }
